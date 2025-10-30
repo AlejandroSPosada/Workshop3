1ea8fd71b5b5c0fb5362fa61461a04ee018e297a
AlejandroSPosada <asepulvedp@eafit.edu.co>
2025-10-30 12:25:37 -0500

    ALLOWED HOSTS modified

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-wb$7fw=)iuwtia2))d!^@byjkpv#&)yf@w7c*1p4i7u9mjtmz*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.222.161.148']
 
 
 # Application definition
